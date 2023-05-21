72726972efd525eebc70ddfd6f2254d370933f53
Mary <maryandreeva100@gmail.com>
2023-05-22 01:30:49 +0300

    Add information

--- templates/information.php
+++ templates/information.php
@@ -9,102 +9,201 @@ get_header(); ?>
 			<!-- Кованная полоса -->
       <div class="row information_title">
         <div class="col-12">
-          <h2>Кованная полоса</h2>
+          <h2><?= get_category(7, ARRAY_A)['name']; ?></h2>
         </div>
       </div>
 			<div class="row information_grid">
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/1/1.jpg">
-					<h3>ГЛАДКАЯ ПОЛОСА</h3>  
-				</div>
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/1/2.jpg">
-					<h3>ЛОЗА ВИНОГРАДА</h3>  
-				</div>
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/1/3.jpg">
-					<h3>ГРЕЧЕСКАЯ ПОЛОСА</h3>  
-				</div>
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/1/4.jpg">
-					<h3>РАКУШКА</h3>  
-				</div>
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/1/5.jpg">
-					<h3>КОРА ДЕРЕВА</h3>  
-				</div>
+			<?php 
+        $posts = get_posts( [
+          'numberposts' => -1,
+          'category' => 7,     
+          'orderby' => 'title', 
+          'order' => 'ASC',
+          'post_type' => 'post',
+          'suppress_filter' => true 
+        ]);
+        foreach($posts as $post) {
+          setup_postdata($post);
+          ?>
+					<div class="col-3 col-lg-6 col-sm-12">
+						<?php the_post_thumbnail(''); ?> 
+						<h3><?php the_title(); ?></h3>  
+					</div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
 			</div>
 			<!-- Ручки -->
 			<div class="row information_title">
         <div class="col-12">
-          <h2>Ручки</h2>
+          <h2><?= get_category(8, ARRAY_A)['name']; ?></h2>
         </div>
       </div>
 			<div class="row information_grid">
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/2/1.jpg">
-					<h3>BAROCCO SM AB-7 МАТОВАЯ БРОНЗА</h3>  
-				</div>
+			<?php 
+        $posts = get_posts( [
+          'numberposts' => -1,
+          'category' => 8,      
+          'orderby' => 'title', 
+          'order' => 'ASC',
+          'post_type' => 'post',
+          'suppress_filter' => true 
+        ]);
+        foreach($posts as $post) {
+          setup_postdata($post);
+          ?>
+					<div class="col-3 col-lg-6 col-sm-12">
+						<?php the_post_thumbnail(''); ?> 
+						<h3><?php the_title(); ?></h3>  
+					</div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
 			</div>
+
 			<!-- Элементы ковки -->
 			<div class="row information_title">
         <div class="col-12">
-          <h2>Элементы ковки</h2>
+          <h2><?= get_category(9, ARRAY_A)['name']; ?></h2>
         </div>
       </div>
 			<div class="row information_grid">
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/3/1.jpg">
-					<h3>19590</h3>  
-				</div>
+			<?php 
+        $posts = get_posts( [
+          'numberposts' => -1,
+          'category' => 9,     
+          'orderby' => 'title', 
+          'order' => 'ASC',
+          'post_type' => 'post',
+          'suppress_filter' => true 
+        ]);
+        foreach($posts as $post) {
+          setup_postdata($post);
+          ?>
+					<div class="col-3 col-lg-6 col-sm-12">
+						<?php the_post_thumbnail(''); ?> 
+						<h3><?php the_title(); ?></h3>  
+					</div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
+			</div>
 			</div>
 			<!-- Цвет -->
 			<div class="row information_title">
         <div class="col-12">
-          <h2>Цвет</h2>
+          <h2><?= get_category(10, ARRAY_A)['name']; ?></h2>
         </div>
       </div>
 			<div class="row information_grid">
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/4/1.jpg">
-					<h3>ЧЕРНОЕ ЗОЛОТО (N24099)</h3>  
-				</div>
+			<?php 
+        $posts = get_posts( [
+          'numberposts' => -1,
+          'category' => 10,      
+          'orderby' => 'title', 
+          'order' => 'ASC',
+          'post_type' => 'post',
+          'suppress_filter' => true 
+        ]);
+        foreach($posts as $post) {
+          setup_postdata($post);
+          ?>
+					<div class="col-3 col-lg-6 col-sm-12">
+						<?php the_post_thumbnail(''); ?> 
+						<h3><?php the_title(); ?></h3>  
+					</div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
+			</div>
 			</div>
 			<!-- Рисунок МДФ 10мм -->
 			<div class="row information_title">
         <div class="col-12">
-          <h2>Рисунок МДФ 10мм</h2>
+          <h2><?= get_category(11, ARRAY_A)['name']; ?></h2>
         </div>
       </div>
 			<div class="row information_grid">
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/5/1.jpg">
-					<h3>№2 (ГЛУХАЯ)</h3>  
-				</div>
+			<?php 
+        $posts = get_posts( [
+          'numberposts' => -1,
+          'category' => 11,      
+          'orderby' => 'title', 
+          'order' => 'ASC',
+          'post_type' => 'post',
+          'suppress_filter' => true 
+        ]);
+        foreach($posts as $post) {
+          setup_postdata($post);
+          ?>
+					<div class="col-3 col-lg-6 col-sm-12">
+						<?php the_post_thumbnail(''); ?> 
+						<h3><?php the_title(); ?></h3>  
+					</div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
 			</div>
 			<!-- Рисунок МДФ 16мм -->
 			<div class="row information_title">
         <div class="col-12">
-          <h2>Рисунок МДФ 16мм</h2>
+          <h2><?= get_category(12, ARRAY_A)['name']; ?></h2>
         </div>
       </div>
 			<div class="row information_grid">
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/6/1.jpg">
-					<h3>№2 (ГЛУХАЯ)</h3>  
-				</div>
+			<?php 
+        $posts = get_posts( [
+          'numberposts' => -1,
+          'category' => 12,      
+          'orderby' => 'title', 
+          'order' => 'ASC',
+          'post_type' => 'post',
+          'suppress_filter' => true 
+        ]);
+        foreach($posts as $post) {
+          setup_postdata($post);
+          ?>
+					<div class="col-3 col-lg-6 col-sm-12">
+						<?php the_post_thumbnail(''); ?> 
+						<h3><?php the_title(); ?></h3>  
+					</div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
 			</div>
 			<!-- Цвета МДФ -->
 			<div class="row information_title">
         <div class="col-12">
-          <h2>Цвета МДФ</h2>
+          <h2><?= get_category(13, ARRAY_A)['name']; ?></h2>
         </div>
       </div>
 			<div class="row information_grid">
-				<div class="col-3 col-lg-6 col-sm-12">
-					<img src="img/decoration/7/1.jpg">
-					<h3>БЕЛОЕ ДЕРЕВО</h3>  
-				</div>
+			<?php 
+        $posts = get_posts( [
+          'numberposts' => -1,
+          'category' => 13,      
+          'orderby' => 'title', 
+          'order' => 'ASC',
+          'post_type' => 'post',
+          'suppress_filter' => true 
+        ]);
+        foreach($posts as $post) {
+          setup_postdata($post);
+          ?>
+					<div class="col-3 col-lg-6 col-sm-12">
+						<?php the_post_thumbnail(''); ?> 
+						<h3><?php the_title(); ?></h3>  
+					</div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
 			</div>
     </div>
   </div>
